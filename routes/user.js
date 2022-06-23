17602787ae882de6822c8893e2e01dcda08de865
Jonathan.P <yonman2@gmail.com>
2022-06-23 10:51:41 +0300

    More fixes

--- routes/user.js
+++ routes/user.js
@@ -44,7 +44,7 @@ router.get('/favorites', async (req,res,next) => {
     let favorite_recipes = {};
     const recipes_id = await user_utils.getFavoriteRecipes(user_id);
     let recipes_id_array = [];
-    recipes_id.map((element) => recipes_id_array.push(element.recipe_id)); //extracting the recipe ids into array
+    recipes_id.map((element) => recipes_id_array.push(element)); //extracting the recipe ids into array
     const results = await recipe_utils.getRecipesPreview(recipes_id_array);
     res.status(200).send(results);
   } catch(error){
--- routes/utils/user_utils.js
+++ routes/utils/user_utils.js
@@ -6,12 +6,12 @@ async function markAsFavorite(user_id, recipe_id){
 }
 
 async function getFavoriteRecipes(user_id){
-    const recipes_id = await DButils.execQuery(`select recipe_id from favorite_recipes where user_id='${user_id}'`);
+    const recipes_id = await DButils.execQuery(`select recipe_id as id from favorite_recipes where user_id='${user_id}'`);
     return recipes_id;
 }
 
 async function getLastSeenRecipes(user_id){
-    const recipes_id = await DButils.execQuery(`select recipe_id from user_recipe_seen_date where user_id='${user_id}' ORDER BY seen_date DESC LIMIT 3`);
+    const recipes_id = await DButils.execQuery(`select recipe_id as id from user_recipe_seen_date where user_id='${user_id}' ORDER BY seen_date DESC LIMIT 3`);
     return recipes_id;
 }
 
